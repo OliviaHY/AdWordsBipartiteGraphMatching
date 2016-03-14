40f52e831acdc71d001350fd59c0c519444af4bd
Olivia Huang <oliviahyacinth@gmail.com>
2016-03-14 14:54:07 -0400

    greedy done

--- adwords.py
+++ adwords.py
@@ -1,38 +1,79 @@
+import sys
 import csv
 import numpy as np
+import random
 from collections import defaultdict
 
-#ifile  = open('bidder_dataset.csv', "rb")
 
-#matrix = np.genfromtxt('bidder_dataset.csv', delimiter=",",skiprows=0,dtype= None,
-						#names = ('Advertiser','Keyword','Bid Value','Budget'))
+def availble(l,bid):
+	flag = True
+	for elmt in l:
+		if elmt < bid:
+			flag = False
+		else:
+			flag = True
+	return flag
 
-#matrix = np.loadtxt('bidder_dataset.csv', delimiter=",",skiprows=1,
-	#dtype= {'names': ('Advertiser','Keyword','Bid Value'),
-	#'formats':(np.int, '|S15', np.float)},usecols=[0,1,2])
-matrix = np.loadtxt('bidder_dataset.csv', delimiter=",",skiprows=1,
+def greedy(queries, kywds2advtser, bids, budgets):
+	result = {}
+	revenue = 0
+	for query in queries:
+		allbudgets = []
+		allbids = []
+		winner = None
+		highest = 0
+		#print kywds2advtser[query]
+		for advertiser in kywds2advtser[query]:
+			key = (advertiser,query)	
+			allbudgets.append(budgets[advertiser])
+			if  all(budget <=0 for budget in allbudgets):
+				break	
+			if bids[key] <= budgets[advertiser]:
+				allbids.append(bids[key])
+				if bids[key] > highest:
+					highest = bids[key]
+					winner = advertiser
+		if winner != None:
+			newbudget = budgets[winner] - bids[(winner,query)]
+			del(budgets[winner])
+			budgets[winner] = newbudget
+		result[query]=winner
+		revenue += highest
+	return revenue
+
+def main():
+	matrix = np.loadtxt('bidder_dataset.csv', delimiter=",",skiprows=1,
 	dtype= '|S15')
-print matrix
-budgets  = dict((int(i[0]),int(i[3])) for i in matrix if i[3]!='')
-#budgets = budgets.astype(np.int)
-#print budgets
-budgets  = dict((int(i[0]),int(i[3])) for i in matrix if i[3]!='')
-queries = np.loadtxt('queries.txt', delimiter="\n", dtype= '|S15')
-bids  = dict(((int(i[0]),i[1]),float(i[2])) for i in matrix)
-print bids
+	#print matrix.shape
+	budgets  = dict((int(i[0]),int(i[3])) for i in matrix if i[3]!='')
+	bids  = dict(((int(i[0]),i[1]),float(i[2])) for i in matrix)
+	#print bids
 
-#print queries
-templist  = list((i[1],int(i[0])) for i in matrix)
-tempdict = defaultdict(list)
-for k,v in templist:
-	tempdict[k].append(v)
-#print tempdict
-queries = np.loadtxt('queries.txt', delimiter="\n", dtype= '|S15')
+	#print queries
+	templist  = list((i[1],int(i[0])) for i in matrix)
+	kywds2advtser = defaultdict(list)
+	for k,v in templist:
+		kywds2advtser[k].append(v)
+	#print kywds2advtser
+	queries = np.loadtxt('queries.txt', delimiter="\n", dtype= '|S15')
 
-# for query in queries:
-# 	i = 0
-# 	for advertiser in matrix[:,0]:
-# 		if query == matrix[i,1]:
-# 			print matrix[i,:]
-# 		i+=1
+	totalBudget = sum(budgets.values())
+	method = sys.argv[1]
+	if method.lower() == 'greedy':
+		revenues = []
+		for i in range(0,100):
+			random.shuffle(queries)
+			temp = greedy(queries, kywds2advtser, bids, budgets)
+			budgets  = dict((int(i[0]),int(i[3])) for i in matrix if i[3]!='')
+			#print temp
+			revenues.append(temp)
+		result = sum(revenues)/100
+		competRate = result/totalBudget
+		print result
+		print competRate
+
+if __name__ == "__main__":
+    main()
+	
+
 
