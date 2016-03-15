ba6f3c6d5a893501fdbdbd7a818532a379143b41
Olivia Huang <oliviahyacinth@gmail.com>
2016-03-15 13:20:34 -0400

    msvv done

--- adwords.py
+++ adwords.py
@@ -3,6 +3,7 @@ import csv
 import numpy as np
 import random
 from collections import defaultdict
+import math
 
 
 def availble(l,bid):
@@ -41,6 +42,78 @@ def greedy(queries, kywds2advtser, bids, budgets):
 		revenue += highest
 	return revenue
 
+def msvv(queries, kywds2advtser, bids, budgets):
+	result = {}
+	revenue = 0
+	budgets0 = budgets.copy()
+	for query in queries:
+		allbudgets = []
+		allbids = []
+		winner = None
+		highest = 0
+		temp = 0.0
+		#print kywds2advtser[query]
+		for advertiser in kywds2advtser[query]:
+			key = (advertiser,query)	
+			allbudgets.append(budgets[advertiser])
+			if  all(budget <=0 for budget in allbudgets):
+				break
+			if bids[key] <= budgets[advertiser]:
+				allbids.append(bids[key])
+				xu = 1-float(budgets[advertiser])/budgets0[advertiser]
+				y = bids[key]*(1-math.exp(xu-1))
+				print xu,y
+				if y >= temp:
+					print 'here'
+					temp = y
+					winner = advertiser
+					highest = bids[key]
+				print y
+		if winner != None:
+			newbudget = budgets[winner] - bids[(winner,query)]
+			del(budgets[winner])
+			budgets[winner] = newbudget
+		result[query]=winner
+		revenue += highest
+	print result
+	return revenue
+
+def balance(queries, kywds2advtser, bids, budgets):
+	result = {}
+	revenue = 0
+	budgets0 = budgets.copy()
+	for query in queries:
+		allbudgets = []
+		allbids = []
+		winner = None
+		highest = 0
+		temp = 0.0
+		#print kywds2advtser[query]
+		for advertiser in kywds2advtser[query]:
+			key = (advertiser,query)	
+			allbudgets.append(budgets[advertiser])
+			if  all(budget <=0 for budget in allbudgets):
+				break
+			if bids[key] <= budgets[advertiser]:
+				allbids.append(bids[key])
+				xu = 1-float(budgets[advertiser])/budgets0[advertiser]
+				y = bids[key]*(1-math.exp(xu-1))
+				print xu,y
+				if y >= temp:
+					print 'here'
+					temp = y
+					winner = advertiser
+					highest = bids[key]
+				print y
+		if winner != None:
+			newbudget = budgets[winner] - bids[(winner,query)]
+			del(budgets[winner])
+			budgets[winner] = newbudget
+		result[query]=winner
+		revenue += highest
+	print result
+	return revenue
+
 def main():
 	matrix = np.loadtxt('bidder_dataset.csv', delimiter=",",skiprows=1,
 	dtype= '|S15')
@@ -59,15 +132,28 @@ def main():
 
 	totalBudget = sum(budgets.values())
 	method = sys.argv[1]
+	runnumber = 5
 	if method.lower() == 'greedy':
 		revenues = []
-		for i in range(0,100):
+		for i in range(0,runnumber):
 			random.shuffle(queries)
 			temp = greedy(queries, kywds2advtser, bids, budgets)
 			budgets  = dict((int(i[0]),int(i[3])) for i in matrix if i[3]!='')
 			#print temp
 			revenues.append(temp)
-		result = sum(revenues)/100
+		result = sum(revenues)/runnumber
+		competRate = result/totalBudget
+		print result
+		print competRate
+	elif method.lower() == 'msvv':
+		revenues = []
+		for i in range(0,runnumber):
+			random.shuffle(queries)
+			temp = msvv(queries, kywds2advtser, bids, budgets)
+			budgets  = dict((int(i[0]),int(i[3])) for i in matrix if i[3]!='')
+			#print temp
+			revenues.append(temp)
+		result = sum(revenues)/runnumber
 		competRate = result/totalBudget
 		print result
 		print competRate
